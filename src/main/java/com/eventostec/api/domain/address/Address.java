e5f93ddbe88170ef2ca9b2db598a9f56821ac1be
Ranieri <rani_pioner@hotmail.com>
2025-03-13 14:48:51 -0300

    Creating entities

---
+++ src/main/java/com/eventostec/api/domain/address/Address.java
@@ -0,0 +1,26 @@
+package com.eventostec.api.domain.address;
+
+import java.util.UUID;
+
+import com.eventostec.api.domain.event.Event;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "address")
+public class Address {
+	@Id
+	@GeneratedValue
+	private UUID id;
+	private String city;
+	private String uf;
+	
+	@ManyToOne
+	@JoinColumn(name = "event_id")
+	private Event event;
+}
